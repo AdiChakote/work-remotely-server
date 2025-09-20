8a5787cd9445022dcf1c3ae23078be67f032f62b
AdiChakote <adichakote@gmail.com>
2025-09-20 07:58:38 +0530

    backend structure created

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Backend running"));
+
+io.on("connnection", (socket) => {
+  console.log("User connected:", socket.id);
+  socket.on("disconnect", () => console.log("User disconnected:", socket.id));
+});
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => console.log(`server running on ${PORT}`));
